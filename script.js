4db67ecd39bdc5886151c2c235c59082f6b9c5c9
tiger <biguptiger@gmail.com>
2023-06-23 20:28:45 +0800

    ファイルの読み込み

--- script.js
+++ script.js
@@ -1,44 +1,33 @@
 let OriginImg = document.getElementById('origin-img');
-let canvas = document.getElementById('canvas');
 
-// ドラッグ開始
-// ここで定義されているfunctionは無名関数と呼ばれる
-// eは引数で、イベントオブジェクトを参照している
-// イベントが発生したときの詳細情報を持っている
-
+// dragover時のイベント設定
 OriginImg.addEventListener('dragover', function(e) {
-    // 以下のスクリプトはe（イベントオブジェクト）のメソッドやプロパティを操作してい
-    // preventDefaultとstopPropagationは、ブラウザーの既定の動作やイベントの伝播を防ぐために広く使用されます。
-    e.preventDefault();
+    // eventの伝搬停止とブラウザのデフォルト動作の防止
     e.stopPropagation();
-    // ドロップ操作の結果として何が起こるべきかを制御
+    e.preventDefault();
+
+    // ドロップ時の効果を指定するプロパティ：dataTransfer
     e.dataTransfer.dropEffect = 'copy';
-    //　その他のイベントオブジェクトのプロパティ
-    // console.log('Start!!!!!!!!!!!!!!!!!!!');
-    // console.log('e.target: ', e.target);
-    // console.log('e.target.id: ', e.target.id);
-    // console.log('e.currentTarget: ', e.currentTarget);
-    // console.log('e.bubbles: ', e.bubbles);
-    // console.log('e.cancelable: ', e.cancelable);
-    // console.log('e.timeStamp: ', e.timeStamp);
-    // console.log('Finish!!!!!!!!!!!!!!!!!!!');
 });
 
-// 'drop'イベントを対象（target）に追加します。これはユーザーがファイルをドロップしたときに発火します。
+// drop時のイベント設定
 OriginImg.addEventListener('drop', function (e) {
-    // イベントの伝播を止めます。これにより、親要素へのイベントの伝播が防がれます。
-	e.stopPropagation();
-    // ブラウザのデフォルトの動作（この場合、ファイルを新しいタブで開くなど）を防ぎます。
+    // eventの伝搬停止とブラウザのデフォルト動作の防止
+    e.stopPropagation();
 	e.preventDefault();
-    // 新しいFileReaderオブジェクトを作成します。これは、ファイルの読み取りを非同期に行うためのAPIです。
+    // pタグにDOM操作で画像名をテキストに指定
+    document.getElementById('origin-img-name').textContent = e.dataTransfer.files[0].name;
+	
+    // ファイルの非同期読み込みを行うためのFileReaderオブジェクトの作成
 	const reader = new FileReader();
-    // ファイルが読み込まれたときに発火するイベントハンドラーを設定します。
+
+    // 読み込み完了時の処理
 	reader.onload = function (e) {
-        // 画像のプレビュー要素（idが'preview'の要素）のsrc属性に、読み込まれたファイルのデータを設定します。
 		document.getElementById('preview').src = e.target.result;
-	}
-    // ドロップされたファイル（ここでは最初のファイル）を読み込みます。読み込みはデータURLとして行われます。
+        document.getElementById('preview').width = "300";
+        document.getElementById('preview').height = "300";
+	};
+    // ファイルの非同期読み込み
 	reader.readAsDataURL(e.dataTransfer.files[0]);
-    console.log(e.target.result);
 });
 
