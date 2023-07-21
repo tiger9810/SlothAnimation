a9ddbb4d94d957dcf2a9d076b4fd2ba7a6eb4051
tiger98 <biguptiger@gmail.com>
2023-07-21 16:30:18 +0900

    Tailwind CSSで画像とボタンを揃えた

--- script.js
+++ script.js
@@ -84,6 +84,9 @@ getId("file-input").addEventListener('change', function(e) {
     const reader = new FileReader();
     reader.onload = function (e) {
         const imgDataURL = e.target.result;
+        // pタグにDOM操作でpixel sizeをテキストに指定
+        setPixelText('mosaic-dot-value', 'pixel dot:12');
+        setPixelText('animation-value', 'moved:20');
         img.onload = function() {
             // Canvasに描画する関数
             let CanvasOrigin = drawCanvas("canvas-origin");
@@ -94,15 +97,11 @@ getId("file-input").addEventListener('change', function(e) {
         const OriginImg = drawCanvas('canvas-origin').canvas;
         const OriginImgCtx = drawCanvas('canvas-origin').ctx;
         OriginCanvasImgData = OriginImgCtx.getImageData(0, 0, OriginImg.width, OriginImg.height);
-
     }
     // ファイルの非同期読み込み
     reader.readAsDataURL(e.target.files[0]);
-    // pタグにDOM操作で画像名をテキストに指定
-    getId('origin-img-name').textContent = e.target.files[0].name;
-    // pタグにDOM操作でpixel sizeをテキストに指定
-    setPixelText('mosaic-dot-value', 'pixel size:12');
-    setPixelText('animation-value', 'animation:20');
+    // // pタグにDOM操作で画像名をテキストに指定
+    // getId('origin-img-name').textContent = e.target.files[0].name;
 });
 
 
